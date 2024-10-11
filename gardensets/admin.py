29ece99c8da5d340b83c4879b1e7f27aa200ed5b
Maribel Pajares <qiangrenlupu@gmail.com>
2024-10-11 14:53:38 +0000

    Customized admin

--- gardensets/admin.py
+++ gardensets/admin.py
@@ -2,5 +2,25 @@ from django.contrib import admin
 from .models import Gardenset, Category
 
 # Register your models here.
-admin.site.register(Gardenset)
-admin.site.register(Category)
+
+class GardensetAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'basic_name',
+        'name',
+    )
+
+
+admin.site.register(Gardenset,GardensetAdmin)
+admin.site.register(Category, CategoryAdmin)
