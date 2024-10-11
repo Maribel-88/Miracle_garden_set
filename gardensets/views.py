480c6f6f532b2c378d0cbb2e45b583c3b8245c63
Maribel Pajares <qiangrenlupu@gmail.com>
2024-10-11 15:45:50 +0000

    Added gardensets views and templates

--- gardensets/views.py
+++ gardensets/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Gardenset
 
 # Create your views here.
+
+def all_garden_sets(request):
+    """ A view to return all garden sets, including sorting and search queries """
+
+    gardensets = Gardenset.objects.all()
+
+    context = {
+        'gardensets': gardensets,
+    }
+    return render(request, 'gardensets/gardensets.html', context)
