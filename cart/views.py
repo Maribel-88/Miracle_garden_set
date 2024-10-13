d9600a8b0853204766e089252f06edb9b2992d43
Maribel Pajares <qiangrenlupu@gmail.com>
2024-10-13 17:09:12 +0000

    Added toast notification functionality

--- cart/views.py
+++ cart/views.py
@@ -13,14 +13,17 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
+    gardenset = get_object_or_404(Gardenset, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
     if item_id in list(cart.keys()):
         cart[item_id] += quantity
+        messages.success(request, f'Updated {gardenset.name} quantity to {cart[item_id]}')
     else:
         cart[item_id] = quantity
+        messages.success(request, f'Added {gardenset.name} to your cart')
 
     request.session['cart'] = cart
     return redirect(redirect_url)
