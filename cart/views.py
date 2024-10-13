8876ab65b5ba0af49102849166c8821bb2989113
Maribel Pajares <qiangrenlupu@gmail.com>
2024-10-13 15:23:28 +0000

    Updated the quantity button functionality in shopping cart page

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.contrib import messages
+
+from gardensets.models import Gardenset 
 
 # Create your views here.
 
@@ -21,4 +24,37 @@ def add_to_cart(request, item_id):
 
     request.session['cart'] = cart
     return redirect(redirect_url)
+
+def update_cart(request, item_id):
+    """ Adjust the quantity of the specified property to the shopping cart """
+
+    gardenset = get_object_or_404(Gardenset, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[item_id] = quantity
+        messages.success(request, f'Updated {gardenset.name} quantity to {cart[item_id]}')
+    else:
+        cart.pop(item_id)
+        messages.success(request, f'Removed {gardenset.name} from your cart')
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
+
+
+def remove_from_cart(request, item_id):
+    """Remove the item from the shopping cart """
+
+    try:
+        gardenset = get_object_or_404(Gardenset, pk=item_id)
+        cart = request.session.get('cart', {})
+        cart.pop(item_id)
+        messages.success(request, f'Removed {gardenset.name} from your cart')
+
+        request.session['cart'] = cart
+        return HttpResponse(status=200)
     
+    except Exception as e:
+        messages.error(request,f'Error removing item: {e}')
+        return HttpResponse(status=500)
