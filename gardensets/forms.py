d78734eef8753a2b8a3bdff61a7e92d2cb6ee4aa
Maribel Pajares <qiangrenlupu@gmail.com>
2024-10-20 22:48:59 +0000

    Fixing the widgets

--- gardensets/forms.py
+++ gardensets/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .widgets import CustomClearableFileInput
 from .models import Gardenset, Category
 
 
@@ -7,6 +8,8 @@ class GardenForm(forms.ModelForm):
     class Meta:
         model = Gardenset
         fields = '__all__'
+
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
