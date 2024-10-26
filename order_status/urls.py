2333e805df1a83aefcd634eea2342e284c241d57
Maribel Pajares <qiangrenlupu@gmail.com>
2024-10-26 21:09:29 +0000

    Update order_status html and views

--- order_status/urls.py
+++ order_status/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.order_status, name='order_status')
+    path('', views.order_status, name='order_status'),
 ]
--- order_status/views.py
+++ order_status/views.py
@@ -1,10 +1,32 @@
-from django.shortcuts import render, get_object_or_404
-
+from django.shortcuts import render,redirect,reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from checkout.models import Order
 
 # Create your views here.
 
+
 def order_status(request):
     """ A view to return the index page """
+
+    orderlists = Order.objects.all()
+    query = None
+
+    if 'q' in request.GET:
+        query = request.GET['q']
+        if not query:
+            messages.error(request, "No search result")
+            return redirect (reverse('order_status'))
+        
+        queries = Q(order_number__icontains=query) 
+        orderlists = orderlists.filter(queries)
+
    
-    return render(request, 'order_status/order_status.html')
+    context = {
+        'orderlists': orderlists,
+        'search_term': query,
+    }
+    return render(request, 'order_status/order_status.html', context)
+
+
+    
