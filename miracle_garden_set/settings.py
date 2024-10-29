29b1a48f7ce6261ab7c851f8524b5dba4feafb44
Maribel Pajares <qiangrenlupu@gmail.com>
2024-10-29 21:48:45 +0000

    trying to push staticfiles to S3

--- miracle_garden_set/settings.py
+++ miracle_garden_set/settings.py
@@ -182,7 +182,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'miraclegarden-set'
+    AWS_STORAGE_BUCKET_NAME = 'mymiraclegarden-set'
     AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
