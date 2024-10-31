8a0f32b6b53a9daad6225f4a98da42889e32ef38
Maribel Pajares <qiangrenlupu@gmail.com>
2024-10-31 21:59:47 +0000

    pushing staticfiles

--- miracle_garden_set/settings.py
+++ miracle_garden_set/settings.py
@@ -174,8 +174,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = AWS_S3_CUSTOM_DOMAIN + '/static/' 
+STATIC_URL = '/static/' 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
