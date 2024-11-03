8d06cb13ace159da83fe78727d231fc60f085f0a
Maribel Pajares <qiangrenlupu@gmail.com>
2024-11-03 17:28:05 +0000

    added new secret key

--- miracle_garden_set/settings.py
+++ miracle_garden_set/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = os.environ.get('SECRET_KEY', '')
-SECRET_KEY = '$f#_u1(1e=ubo1n=#2sn@v5(amp!d255bcj(3x=4v(0=rt6ilo'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-#'DEVELOPMENT' in os.environ
+DEBUG = 'DEVELOPMENT' in os.environ
+
 
 ALLOWED_HOSTS = ['*']
 
